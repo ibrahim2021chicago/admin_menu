2442d22f9301d89fcae8a93ba6f4160b3d5b46ac
root <root@Exfinity.localdomain>
2022-11-29 22:18:54 +0530

    first commit

---
+++ optix_admin.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo ""
+echo ""
+echo "OPTIX Administration menu"
+echo "-------------------------"
+echo ""
+echo `hostname`
+echo "DR Site: WTC"
+echo ""
+echo "1. OPTIX Server Admin"
+echo "2. Mail Extractor Process Admin"
+echo "3. Apache Web Server Admin"
+echo "4. DR State Admin"
+echo "5. Show Support Notes"
