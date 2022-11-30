8a047464a030e4c1ea5e524d018819125df7338f
root <root@Exfinity.localdomain>
2022-11-30 21:08:55 +0530

    read input

--- optix_admin.sh
+++ optix_admin.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-echo ""
-echo ""
-echo "OPTIX Administration menu"
-echo "-------------------------"
-echo ""
+echo "
+
+OPTIX Administration menu
+-------------------------
+"
 echo `hostname`
-echo "DR Site: WTC"
-echo ""
-echo "1. OPTIX Server Admin"
-echo "2. Mail Extractor Process Admin"
-echo "3. Apache Web Server Admin"
-echo "4. DR State Admin"
-echo "5. Show Support Notes"
+echo "DR Site: WTC
+
+1. OPTIX Server Admin
+2. Mail Extractor Process Admin
+3. Apache Web Server Admin
+4. DR State Admin
+5. Show Support Notes
+Choose an option [1-5]: "
+read option
+case $option in
+1)OPTIX Server Admin;;
+esac
