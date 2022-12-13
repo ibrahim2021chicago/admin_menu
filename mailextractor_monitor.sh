2a8e9d30823be1786728d881b84724f58dd1e2de
Mohammed Ibrahim Khaleel (OSV) <f4797019@vwb22002.ute.fedex.com>
2022-12-13 21:12:06 +0000

    mail subject changed

--- mailextractor_monitor.sh
+++ mailextractor_monitor.sh
@@ -3,11 +3,11 @@
 email=mohammed.khaleel.osv@fedex.com
 host=`hostname -f`
 ps_out=`ps -ef | grep dailywork-config | grep -v grep`;
-subject="Mail Extractor service at $host has been restarted"
+subject="Mail Extractor service alert"
 
 if [[ "$ps_out" != "" ]];then
 echo "Service is running"
 else
 sudo -u optix /opt/software/optix/optix6/bin/runDailyWorkMailDir.sh
-echo "Mail Extractor service at $host wasn't running and has been restarted" | mail -s "$subject" $email
+echo "Mail Extractor service at $host wasn't running and has been restarted." | mail -s "$subject" $email
 fi
