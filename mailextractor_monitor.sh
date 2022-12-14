17f2b9a3e93e409d2a3e323bc51bfc04dc5bcad4
ibrahim2021chicago <ibrahim2021chicago@gmail.com>
2022-12-15 02:38:08 +0530

    added config for mailextractor monitoring

--- mailextractor_monitor.sh
+++ mailextractor_monitor.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-email=mohammed.khaleel.osv@fedex.com
 host=`hostname -f`
 ps_out=`ps -ef | grep dailywork-config | grep -v grep`;
-subject="Mail Extractor service alert"
 
 if [[ "$ps_out" != "" ]];then
 echo "Service is running"
 else
-sudo -u optix /opt/software/optix/optix6/bin/runDailyWorkMailDir.sh
+sudo -u optix $mail_extractor/runDailyWorkMailDir.sh
 echo "Mail Extractor service at $host wasn't running and has been restarted." | mail -s "$subject" $email
 fi
